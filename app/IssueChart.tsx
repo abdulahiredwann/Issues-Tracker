63b2c45497870698ae19bf6ca423786e33f1a9e9
Abdulahi <123581480+abdulahiredwann@users.noreply.github.com>
2024-07-21 17:04:35 +0300

    Error Handling ,Finshed Project

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -2,28 +2,39 @@
 import { Card } from "@radix-ui/themes";
 import React from "react";
 import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+
 interface Props {
   open: number;
   inProgress: number;
   close: number;
 }
+
+function CustomYAxis(props: any) {
+  return <YAxis {...props} />;
+}
+
+function CustomXAxis(props: any) {
+  return <XAxis {...props} />;
+}
+
 function IssueChart({ close, inProgress, open }: Props) {
   const data = [
     { label: "Open", value: open },
     { label: "In Progress", value: inProgress },
     { label: "Closed", value: close },
   ];
+
   return (
     <Card>
       <ResponsiveContainer width={"100%"} height={300}>
         <BarChart data={data}>
-          <XAxis dataKey={"label"}></XAxis>
-          <YAxis></YAxis>
+          <CustomXAxis dataKey="label" />
+          <CustomYAxis />
           <Bar
             style={{ fill: "var(--accent-9)" }}
-            barSize={"60"}
-            dataKey={"value"}
-          ></Bar>
+            barSize={60}
+            dataKey="value"
+          />
         </BarChart>
       </ResponsiveContainer>
     </Card>
